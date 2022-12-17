3fa26c351c94fb21f04eeaadf93faccfbf5739b2
khizer406-git <khizerjilani@gmail.com>
2022-12-17 22:29:38 +0500

    Image added & Landing page 2 designed

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,17 +4,19 @@ import LandingPage from './Pages/LandingPage';
 import Sidebar from './Components/Sidebar';
 import Home from './Pages/Doctors/Home';
 import Recommendation from './Pages/Doctors/Recommendation';
-
+import RegisterComponent from "./Components/LandingPage/register";
 
 
 function App() {
   return (
+      // @Sarmad add this in Route <RegisterComponent/>
    <BrowserRouter>
    <Routes>
     <Route path="/" element={<LandingPage/>}></Route>
     <Route path="/new" element={<Sidebar/>}></Route>
     <Route path="/doctor" element={<Home/>}></Route>
     <Route path="/recommendation" element={<Recommendation/>}></Route>
+    <Route path="/" element={<RegisterComponent/>}></Route>
    </Routes>
    </BrowserRouter>
   );
