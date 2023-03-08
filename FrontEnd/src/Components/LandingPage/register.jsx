981f82d876d2147c4ba79ba91ea22ee044011267
khizer406-git <khizerjilani@gmail.com>
2023-03-08 09:12:18 +0500

    Collaboration Sidebar BC

--- FrontEnd/src/Components/LandingPage/register.jsx
+++ FrontEnd/src/Components/LandingPage/register.jsx
@@ -6,18 +6,24 @@ import { useContext, useEffect, useState } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { Link } from "react-router-dom";
 import { ethers } from "ethers";
-import {abi} from "../../Contracts/abi";
+import { abi } from "../../Contracts/abi";
+import axios from "axios";
 
 const RegisterComponent = () => {
+  const API_KEY = "c303a8d05f40b047e81f";
+  const API_Secret =
+    "869916f5afead162492bf1d096a41082e189f911afc0e007b9f2feee7a56abc5";
+  const JWT_token =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxZWY5NzU1Yy02NjNjLTRkMDUtYTY1NS1kNjRmZjFlOGM2YWIiLCJlbWFpbCI6ImtoaXplcmppbGFuaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZDRlMzlkMzhhODgyOTVkNWJiMDkiLCJzY29wZWRLZXlTZWNyZXQiOiJlYjAzZDhlZTM4NDRiZDIwN2RiYzg3MDQ5M2FlMjlmMmE2NmMwZTEyMzU0MGIxZWIzNjU3MmZhMGNhMGJkNDUyIiwiaWF0IjoxNjc4MTk5MDg1fQ.2CJ7itaFc-K8VX7GbQqiu1VzSkrh68O3TfYH2IwJY8M";
   const [add, setAdd] = useState("");
-  const [formData,setFormData] = useState({
-    name: '',
-    email: '',
-    country: '',
-    city: '',
-    department: '',
-    doctors: ''
-  }); 
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    country: "",
+    city: "",
+    department: "",
+    doctors: "",
+  });
   const con = useContext(metaContext);
 
   useEffect(() => {
@@ -40,22 +46,50 @@ const RegisterComponent = () => {
     window.location.reload(false);
     // do something with new account here
   }
-  async function registerHospital()
-  {
+  async function registerHospital() {
+    var f = new File([""], "hospitalOne.txt", { type: "text/plain" });
+    let formdata = new FormData();
+    formdata.append("file", f);
+    try {
+      const resFile = await axios({
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        data: formdata,
+        headers: {
+          pinata_api_key: API_KEY,
+          pinata_secret_api_key: API_Secret,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const Contract=new ethers.Contract("0x850b6847086159d5d9031cD9ee41e4872557ef47",abi,signer);
-    let tx
+    const Contract = new ethers.Contract(
+      "0x850b6847086159d5d9031cD9ee41e4872557ef47",
+      abi,
+      signer
+    );
+    let tx;
     try {
-      tx=await Contract.registerHospital(formData.name,formData.city,formData.email,formData.department,formData.doctors,add);
+      tx = await Contract.registerHospital(
+        formData.name,
+        formData.city,
+        formData.email,
+        formData.department,
+        formData.doctors,
+        add
+      );
       console.log(tx);
     } catch (error) {
-    console.log(error);
+      console.log(error);
     }
   }
 
-  function handleChange(e){
-    setFormData({...formData,[e.target.name]:e.target.value})
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   }
   console.log(formData);
 
@@ -81,44 +115,94 @@ const RegisterComponent = () => {
         </span>
         <span id="reg_input_box">
           <span className="reg_input_upper_box">
-            <label id="reg_label" htmlFor="">Name</label>
-            <input name="name" id="input_special" type="text" onChange={handleChange} />
+            <label id="reg_label" htmlFor="">
+              Hospital Name
+            </label>
+            <input
+              name="name"
+              id="input_special"
+              type="text"
+              onChange={handleChange}
+            />
           </span>
           <span className="reg_input_lower_box">
-            <label id="reg_label" htmlFor="">MetaMask Address</label>
-            <input id="input_special" type="text" defaultValue={add} readOnly></input>
+            <label id="reg_label" htmlFor="">
+              Hospital wallet Address
+            </label>
+            <input
+              id="input_special"
+              type="text"
+              defaultValue={add}
+              readOnly
+            ></input>
           </span>
         </span>
         <span id="reg_input_box">
           <span className="reg_input_upper_box">
-            <label id="reg_label" htmlFor="">Country</label>
-            <input id="input_special" type="text" name="country" onChange={handleChange}/>
+            <label id="reg_label" htmlFor="">
+              Country
+            </label>
+            <input
+              id="input_special"
+              type="text"
+              name="country"
+              onChange={handleChange}
+            />
           </span>
           <span className="reg_input_lower_box">
-            <label id="reg_label" htmlFor="">Department</label>
-            <input id="input_special" type="text" name="department" onChange={handleChange}/>
+            <label id="reg_label" htmlFor="">
+              Phone Number
+            </label>
+            <input
+              id="input_special"
+              type="phone"
+              name="phone"
+              onChange={handleChange}
+            />
           </span>
         </span>
         <span id="reg_input_box">
           <span className="reg_input_upper_box">
-            <label id="reg_label" htmlFor="">Email</label>
-            <input id="input_special" type="email" name="email" onChange={handleChange}/>
+            <label id="reg_label" htmlFor="">
+              Email
+            </label>
+            <input
+              id="input_special"
+              type="email"
+              name="email"
+              onChange={handleChange}
+            />
           </span>
-          <span className="reg_input_lower_box">
-            <label id="reg_label" htmlFor="">Doctors</label>
-            <input id="input_special" type="number" name="doctors" onChange={handleChange}/>
-          </span>
+          {/* <span className="reg_input_lower_box">
+            <label id="reg_label" htmlFor="">
+              Doctors
+            </label>
+            <input
+              id="input_special"
+              type="number"
+              name="doctors"
+              onChange={handleChange}
+            />
+          </span> */}
         </span>
         <span id="reg_input_email">
-          <label id="reg_label" htmlFor="">City</label>
-          <input id="input_special" type="text" name="city" onChange={handleChange}/>
+          <label id="reg_label" htmlFor="">
+            City
+          </label>
+          <input
+            id="input_special"
+            type="text"
+            name="city"
+            onChange={handleChange}
+          />
         </span>
         <Link to="/thanks">
-          <button id="btn_special"
+          <button
+            id="btn_special"
             className="rounded-full bg-limgreen text-sm font-poppins drop-shadow-2xl tracking-widest w-1/7 btn_special"
             onClick={registerHospital}
-         >
-            NEXT 
+          >
+            NEXT
           </button>
         </Link>
       </form>
--- FrontEnd/src/Components/Sidebar/index.jsx
+++ FrontEnd/src/Components/Sidebar/index.jsx
@@ -1,31 +1,60 @@
 import React from 'react'
+import "./index.css"
+import {AiOutlineHome} from 'react-icons/ai'
+import {RiFileCloudLine} from 'react-icons/ri'
+import {MdOutlineFileUpload} from 'react-icons/md'
 
+//918dec bg color
 const Sidebar = () => {
   return (
-    <aside class="w-28 " aria-label="Sidebar">
+    <div className="sidebarDiv">  
+       <ul className="sidebarDivul">
+          <li className='sidebarDivLi'>
+          <AiOutlineHome fontSize={20}/>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Home</a>
+          </li>
+          <li className='sidebarDivLi'>
+            <RiFileCloudLine fontSize={20}/>
+            <a href="http://" target="_blank" rel="noopener noreferrer">Retrive Model </a>
+          </li>
+          <li className='sidebarDivLi'>
+          <MdOutlineFileUpload fontSize={20}/>
+          <a href="http://" target="_blank" rel="noopener noreferrer">Upload Model </a>
+          </li>
+          {/* <li className='sidebarDivLi'>
+          <a href="http://" target="_blank" rel="noopener noreferrer">Home <AiOutlineHome fontSize={20}/></a>
+          </li>
+          <li className='sidebarDivLi'>
+          <a href="http://" target="_blank" rel="noopener noreferrer">Home <AiOutlineHome fontSize={20}/></a>
+          </li> */}
+       </ul>
+    </div>    
+  )
+}
+
+export default Sidebar
+
+
+// eslint-disable-next-line no-lone-blocks
+{/* <aside class="w-28 " aria-label="Sidebar">
     <div class="overflow-y-auto pb-60 pt-20 bg-purple rounded-3xl mx-4">
        <ul class="space-y-8 text-white">
-          <li className='flex justify-center'>
+          <li className='sideabar_li flex justify-center'>
             <span>Home</span>
           </li>
-          <li className='flex justify-center'>
-            <span>Home</span>
+          <li className='sideabar_li flex justify-center'>
+            <span>Retrive Model</span>
           </li>
-          <li className='flex justify-center'>
-            <span>Home</span>
+          <li className='sideabar_li flex justify-center'>
+            <span>Upload Model</span>
           </li>
-          <li className='flex justify-center'>
-            <span>Home</span>
+          <li className='sideabar_li flex justify-center'>
+            <span>FUck</span>
           </li>
-          <li className='flex justify-center'>
+          <li className='sideabar_li flex justify-center'>
             <span>Home</span>
           </li>
          
        </ul>
     </div>
- </aside>
- 
-  )
-}
-
-export default Sidebar
+ </aside> */}
