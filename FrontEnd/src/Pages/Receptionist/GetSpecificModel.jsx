2c038273641ad60f2b0db2e382bee791e40a46c8
MansoorButt <balajmen@gmail.com>
2023-03-20 22:18:19 +0500

    123.

--- FrontEnd/src/Pages/Receptionist/GetSpecificModel.jsx
+++ FrontEnd/src/Pages/Receptionist/GetSpecificModel.jsx
@@ -11,10 +11,10 @@ import mabi from '../../Contracts/model.json'
 import metaContext from '../../context/metaContext'
 import FileSaver from 'file-saver';
 import { useLocation } from 'react-router-dom';
-import "./RetrieveModel.css";
+import "./GetSpecificModel.css";
 
 
-const RetrieveModel = () => {
+const GetSpecificModel = () => {
     const con = useContext(metaContext);
     const [address,setAddress]=useState("");
     const [hospital,setHospital]=useState("");
@@ -60,95 +60,24 @@ const RetrieveModel = () => {
         let allHospitals;
         try {
             
-            allHospitals= await Contract.getHospitals()
+            allHospitals= await Contract.getLatestHashes(location.state)
             setHospital(allHospitals);
-            console.log(hospital.length)
-            if(hospital.length>0){
-                for (let index = 0; index < hospital.length; index++) {
-                  x.push(hospital[index].name);
-                }
-                for (let index = 0; index < hospital.length; index++) {
-                 console.log(x[index])
-                }
-                setfetch(true);
-                
-            }
+            console.log(hospital[0])
+            setipfsHash(hospital[0])
+            setjsonHash(hospital[1])
+           
           } catch (error) {
             console.log(error);
           }
     }
-    async function getVersion(){
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const Contract2 = new ethers.Contract(
-        modelAddress,
-        mabi,
-        signer
-      );
-      console.log("Connected to 2 contract")
-      console.log(Contract2);
-      let tx2
-      try {
-      
-      if(setfetch){  
-      tx2 = await Contract2.LVersion(location.state,selectedOption);
-      console.log("got the Version");
-      console.log(tx2);
-      setversion(parseInt(tx2._hex)-1);
-      console.log(version)}
-        
-      } catch (error) {
-        
-      }
-    }
+    
+    
 
-    async function getHash(){
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const Contract3 = new ethers.Contract(
-        modelAddress,
-        mabi,
-        signer
-      );
-      
-      console.log(Contract3);
-      console.log("Connected to 3 contract")
-      let tx3
-      let tx4
-      try {
-        console.log("Connected to 4 contract")
-      if(setfetch && recieved){
-        console.log("Connected to 5 contract")  
-      tx3 = await Contract3.getLIpfs(location.state)
-      console.log("got the hash");
-      console.log(tx3)
-      tx4 = await Contract3.getLJson(location.state)
-      // console.log(selectedOption)
-      // console.log(version)
-      console.log(tx3);
-      console.log("got the Jsonhash");
-      console.log(tx4)
-      setipfsHash(tx3);
-      setjsonHash(tx4);
-      }
-        
-      } catch (error) {
-        
-      }
-    }
-
-
-    const handleChange = event => {
-      // console.log(event.target.value);
-      setSelectedOption(event.target.value);
-      console.log(selectedOption)
-      getVersion();
-      console.log(version)
-      setrecieved(true);
-          };
 
     
-      const ipfs =""
+
+    
+      
 
       const downloadFileIpfs = () => {
         let ipfsi = "https://gateway.pinata.cloud/ipfs/"
@@ -169,8 +98,10 @@ const RetrieveModel = () => {
       const downloadFileJson = () => {
         let ipfsi = "https://gateway.pinata.cloud/ipfs/"
         jsonHash.trimStart();
-        let substring = jsonHash.slice(1);
+        var substring = jsonHash.slice(1);
         let ipfs = ipfsi+substring;
+        
+
         console.log(ipfs);
         console.log("Hello world json ")
         axios.get(ipfs, {
@@ -185,8 +116,8 @@ const RetrieveModel = () => {
       }
 
       function downloadFile(){
-        // downloadFileIpfs();
-        downloadFileJson();
+        downloadFileIpfs();
+        // downloadFileJson();
       }
     
 
@@ -198,30 +129,17 @@ const RetrieveModel = () => {
       <div className="col-span-10">
           <div className="flex justify-center flex-col items-center space-y-20">
             <div className="bg-gradient-to-r from-gradx1 to-gradx2 text-cdwhite text-2xl mt-8 font-light mr-2 px-12 py-1 rounded-lg tracking-wider font-poppins">
-              {location.state} Model Aggregation
+              {location.state} Model
             </div>
             <div className="text-white" id ="datadetails">
-              <h1>Version : {version}</h1>
+              
               <h1>IPFS Hash : {ipfsHash}</h1>
               <h1>JSON Hash : {jsonHash}</h1>
               {/*  */}
               <div className='datadetailssec2'>
-              <button className="text-white bg-purple p-3 rounded-full" id="btnsp1" onClick={getModel}>Get INDEX</button>
-              <button className="text-white bg-borderPurple p-3 rounded-full" id="btnsp2" onClick={()=>getHash()}>
-              Check Hash
-              </button>
+              <button className="text-white bg-purple p-3 rounded-full" id="btnsp1" onClick={getModel}>Get Hashes</button>
+              
               </div>
-              <div className='selectoptionspd'>
-                  {fetch? (
-                    <select onChange={handleChange} value={selectedOption}>
-                    <option value="">Select an option</option>
-                      {hospital.map((option)=>(
-                        <option key={option} value={option.metamask}>{option.name}</option>
-                      ))}
-                    </select>
-                    ):(<h1>Not</h1>)}  
-                <h1 className="text-white mt-2">{selectedOption}</h1>
-            </div>
               <button className="text-white bg-borderPurple p-3 rounded-full " onClick={downloadFile}>
                 Download File
               </button>
@@ -235,4 +153,4 @@ const RetrieveModel = () => {
   )
 }
 
-export default RetrieveModel;
+export default GetSpecificModel;
--- FrontEnd/src/Pages/superUser/Disease.jsx
+++ FrontEnd/src/Pages/superUser/Disease.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import Navbar from "../../Components/Navbar";
 import Sidebar from "../../Components/Sidebar";
+import {useEffect} from 'react';
+import { useLocation } from 'react-router-dom';
 
-const Disease = () => {
+const Disease = (props) => {
+
+  const location= useLocation();
+  useEffect(()=>{
+
+    console.log(location);
+},[location])
   const [metrics, setMetrics] = useState([
     {
       metric: "Blood Pressure",
@@ -29,14 +37,20 @@ const Disease = () => {
         <div className="col-span-10">
           <div className="flex justify-center mt-4">
             <h1 className=" text-white  border-4 border-purple text-2xl mt-8 font-poppins font-bold mr-2 py-4 px-6 rounded-lg tracking-widest">
-              CardioVascular Disease Model
+              {location.state.name} Disease Model
             </h1>
           </div>
           <div className="flex flex-col justify-center items-center mt-8 ">
             <div className="border-4 border-purple rounded-xl px-16 py-8 flex flex-col gap-y-4">
-              <div className="border-2 border-purple rounded-2xl py-1 px-4">
+              <div className="flex flex-col border-2 border-purple rounded-2xl py-1 px-4">
                 <span className="text-white font-medium font-poppins">
-                  Minimum Accuracy Required : 80%
+                  Current Accuracy : {parseInt(location.state.accuracy._hex)}
+                </span>
+                <span className="text-white font-medium font-poppins">
+                  Version : {parseInt(location.state.version._hex)}
+                </span>
+                <span className="text-white font-medium font-poppins">
+                  Models Combined : {parseInt(location.state.modelsCombined._hex)}
                 </span>
               </div>
               <div className="border-2 border-purple rounded-2xl py-1 px-4">
