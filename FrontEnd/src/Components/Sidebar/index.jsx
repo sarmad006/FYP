5250cbdde21f99cc1cf548fdca4be8014ba85417
khizer406-git <khizerjilani@gmail.com>
2023-03-31 01:48:57 +0500

    . && git push

--- FrontEnd/src/Components/Sidebar/index.jsx
+++ FrontEnd/src/Components/Sidebar/index.jsx
@@ -33,47 +33,13 @@ const Sidebar = () => {
           superUser.map((item) => (
             <li className="sidebarDivLi">
               {item.icon}
-              <a href={item.path}>{item.name}</a>
+              
+               <a href={item.path}>{item.name}</a> 
             </li>
           ))}
-
-        {/* <li className="sidebarDivLi">
-          <RiFileCloudLine fontSize={20} />
-          <a href="/recep/retrieveModel">Retrive Model </a>
-        </li>
-        <li className="sidebarDivLi">
-          <MdOutlineFileUpload fontSize={20} />
-          <a href="/recep/uploadModel">Upload Model </a>
-        </li> */}
       </ul>
     </div>
   );
 };
 
 export default Sidebar;
-
-// eslint-disable-next-line no-lone-blocks
-{
-  /* <aside class="w-28 " aria-label="Sidebar">
-    <div class="overflow-y-auto pb-60 pt-20 bg-purple rounded-3xl mx-4">
-       <ul class="space-y-8 text-white">
-          <li className='sideabar_li flex justify-center'>
-            <span>Home</span>
-          </li>
-          <li className='sideabar_li flex justify-center'>
-            <span>Retrive Model</span>
-          </li>
-          <li className='sideabar_li flex justify-center'>
-            <span>Upload Model</span>
-          </li>
-          <li className='sideabar_li flex justify-center'>
-            <span>FUck</span>
-          </li>
-          <li className='sideabar_li flex justify-center'>
-            <span>Home</span>
-          </li>
-         
-       </ul>
-    </div>
- </aside> */
-}
