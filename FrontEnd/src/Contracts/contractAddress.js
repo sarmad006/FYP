54abc4145d2df7de3e3e34241d3ff48488c71b46
Sarmad Jamal <sarmadjamal2002@gmail.com>
2023-05-01 20:02:46 +0500

    newChanges

--- FrontEnd/src/Contracts/contractAddress.js
+++ FrontEnd/src/Contracts/contractAddress.js
@@ -1,13 +1,13 @@
-export const contractAddress="0x11db3f5068de393b3bD2A144Be97a129e76b3cFa";
-export const modelAddress="0xDf3dE830B7d57cE4dD7594cdb56A3e4A336c1FAb";
-export const hospitalAddress="0xb594B870095B7Bf914064b02267bBC440D9C9bc7";
-export const superuserAddress="0x6326BcC9Df5a8320b47de80580bA5a9C1DcdAF81";
-export const authenticationAdrress = "0xAa8D6A5B7c36efEC0E9Fd51bDe83c6487c55cb18";
-const tokenAddress = "0x6AF1D20A968eFaED25Ed9616B5ECaeB5D93af0ac"; //DOC-Token
+// export const contractAddress="0x11db3f5068de393b3bD2A144Be97a129e76b3cFa";
+// export const modelAddress="0xDf3dE830B7d57cE4dD7594cdb56A3e4A336c1FAb";
+// export const hospitalAddress="0xb594B870095B7Bf914064b02267bBC440D9C9bc7";
+// export const superuserAddress="0x6326BcC9Df5a8320b47de80580bA5a9C1DcdAF81";
+// export const authenticationAdrress = "0xAa8D6A5B7c36efEC0E9Fd51bDe83c6487c55cb18";
+// const tokenAddress = "0x6AF1D20A968eFaED25Ed9616B5ECaeB5D93af0ac"; //DOC-Token
 
-// export const modelAddress="0x1D71411De98837B759e6aDD6Ab152fB93b3E1171";
-// export const hospitalAddress="0xfe2CeEDFBA6AAA7560257189588c95E1b95bFC39";
-// export const superuserAddress="0x655C4E7da2A75Fcfc86Ed3A2A6b03ed5A91A0eb7";
-// export const authenticationAdrress = "0xFA06E560cf301e808F18Bf147294e9EEE1219622";
+export const modelAddress="0x57C31d771996fF5356D688282ebcD56a587a44D4";
+export const hospitalAddress="0xCe3c22Ec5b7Af3ec373514C47b8d0f7aF89126f8";
+export const superuserAddress="0x39e9175e4a43309E95bE0d03252eD1F6Aab19EED";
+export const authenticationAdrress = "0xa1C99ECcA6E212cE79ed87Ab1dC7D6DCbf60DE7F";
 
 //0x11db3f5068de393b3bD2A144Be97a129e76b3cFa aurora testnet
